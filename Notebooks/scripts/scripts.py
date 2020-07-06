9ece6d200a59646822372d819bd9c83a16de7a53
Bryce Haley <bhaley91@gmail.com>
2020-07-06 14:24:42 -0400

    write external py scripts to run the model

--- Notebooks/scripts/scripts.py
+++ Notebooks/scripts/scripts.py
@@ -6,12 +6,19 @@ import os
 import math
 from typing import List, Tuple
 
-def create_tide_plot():
-    """Displays a plot of hourly tide levels for 1 week in May using readings from comox """
+def get_tide_values():
+    """Grabs the tide values measured for one week in comox
+    Returns:
+        a dataframe containing measured tide values for comox"""
+
     tide_path = os.path.join('..', 'resources', 'comox_tide.csv')
     tide_df = pd.read_csv(tide_path)
     tide_df = tide_df.drop(columns = ['PDT'])
-    tide_values = tide_df.values.flatten()
+    return tide_df.flatten() 
+
+def create_tide_plot():
+    """Displays a plot of hourly tide levels for 1 week in May using readings from comox """
+    tide_values = get_tide_values()
     
     seaborn.set()
     plt.style.use('seaborn-deep')
@@ -23,7 +30,7 @@ def create_tide_plot():
     plt.savefig("tide.png")
     plt.show()
 
-def get_ratio_of_perimeter_covered(tide_level: float, perimeter,  radius: int, delta = 5: int) -> float:
+def get_ratio_of_perimeter_covered(tide_level: float, perimeter,  radius = 25: int, delta = 5: int) -> float:
     """Given a tide level and points on the perimeter of a semi-circular trap gives the ratio of the trap under water
 
     Args:
@@ -63,4 +70,78 @@ def get_ratio_of_perimeter_covered(tide_level: float, perimeter,  radius: int, d
     coverage  = angle/ (0.5 * np.pi)
     return coverage
 
+def get_perimeter(radius = 25: int, delta = 5: int, height = 2: float, slope = 0.17: float, intercept = 6: float):
+    """Creates set of points at the top of the semi-circular trap
 
+    Args:
+        radius: an integer value that sets the radius of the trap
+        delta: how far down the y-axis the center of the circle is
+        slope: the slope of the beach on which the trap is built
+        height: the height from the beach surface to the top of the trap
+    
+    returns:
+        the Perimter, a 2d array:
+            [0]: x values
+            [1]: y values
+            [2]: z values
+    """
+    
+    theta = np.linspace(0, np.pi, 100)
+    #equation for a circle
+    x = r * np.cos(theta)
+    y = r * np.sin(theta) + delta
+    # equation for a line
+    z = intercept + height - (slope * y)
+
+    return [x,y,z]
+
+
+def run_trap(tide_level: float, radius: int, delta = 5: int, harvesting = False: bool):
+    """Runs the fish trap model for 1 week.
+
+    Args:
+        tide_level: A tide level reading in meters above sea-level
+        radius: the radius of the semi-circular trap created
+        delta: how far down the y axis the "center" of the semi-circle is from the origin
+        harvesting: if true, user is prompted to harvest some fish at each low tide
+
+    Returns:
+        An 2d array containing:
+            [0]: The total number of harvested fish at hour indexed
+            [1]: The total number of fish in the trap at hour at hour indexed
+            [2]: the total number of fish outside the trap at hour indexed
+    """
+    movement_rate = 0.05
+    max_fish = 1000
+    current_free_fish = max_fish
+    current_caught_fish = 0
+    total_harvested = [0]
+    in_trap = [0]
+    out_trap = [max_fish]
+    perimieter_ratio = (np.pi * r) / (np.pi * 25)
+    tide_values = get_tide_values()
+    
+    perimeter = get_perimeter()
+
+    for level in tide_values:
+        coverage = circ_covered(level, perimeter, radius = 25)
+        free_to_caught = free_fish * coverage * alpha * perimeter_ratio
+        caught_to_free = caught_fish * coverage * alpha * perimeter_ratio
+        current_caught_fish = current_caught_fish - caught_to_free + free_to_caught
+        current_free_fish = current_free_fish + caught_to_free - free_to_caught
+
+        if(coverage > 0):
+            total_harvested.append(total_harvested[-1])
+
+        else:
+            total_harvested.append(total_harvested[-1] + current_caught_fish)
+            if (current_caught_fish != 0):
+                catches.append(current_caught_fish)
+
+            current_caught_fish = 0
+            current_free_fish = max_fish
+
+        in_trap.append(current_caught_fish)
+        out_trap.append(current_free_fish)
+
+    return [total_harvested, in_trap, out_trap]
