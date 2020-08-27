b5beeb22d7382033d76fbbcd55d0448d9fda7a94
Bryce Haley <bhaley91@gmail.com>
2020-08-27 14:41:58 -0400

    tide now moves in and out of animation. Remains to remove fish when trap closes and show only trap parts above water

--- Notebooks/scripts/particle_box.py
+++ Notebooks/scripts/particle_box.py
@@ -17,6 +17,7 @@ import matplotlib.pyplot as plt
 import scipy.integrate as integrate
 import matplotlib.animation as animation
 from IPython.display import HTML
+import pandas as pd
 
 import scripts
 perimeter_raw = get_perimeter(1, 0.08, 0.2, 0.24, 0.0068)
@@ -25,7 +26,7 @@ perimeter_raw = get_perimeter(1, 0.08, 0.2, 0.24, 0.0068)
 class ParticleBox:
     """Orbits class
     
-    init_state is an [N x 4] array, where N is the number of particles:
+    init_state is an [N x 4] array, where N is the number of free_fish:
        [[x1, y1, vx1, vy1],
         [x2, y2, vx2, vy2],
         ...               ]
@@ -36,11 +37,14 @@ class ParticleBox:
                  init_state = [[1, 0, 0, -1],
                                [-0.5, 0.5, 0.5, 0.5],
                                [-0.5, -0.5, -0.5, 0.5]],
-                 init_tide_state = [[-2,2],[2, 2]],
+                 init_tide_state = [[-2,2],[-1, -1]],
                  init_perimeter = [perimeter_raw[0], (perimeter_raw[1] * -1) + 0.2],
                  bounds = [-2, 2, -2, 2],
                  size = 0.04,
-                 M = 0.05):
+                 M = 0.05,
+                 tide_movement_up = True,
+                 end = False,
+                 frozen_count = 0):
         self.init_state = np.asarray(init_state, dtype=float)
         self.init_tide_state = np.asarray(init_tide_state, dtype=float)
         self.init_perimeter = np.asarray(init_perimeter, dtype=float)
@@ -51,52 +55,70 @@ class ParticleBox:
         self.perimeter = self.init_perimeter.copy()
         self.time_elapsed = 0
         self.bounds = bounds
+        self.tide_movement_up = tide_movement_up
+        self.end = end
         
 
     def step(self, dt):
         """step once by dt seconds"""
+        if self.time_elapsed==0:
+            self.bounds[3] = -1
+        
         self.time_elapsed += dt
+        if not self.end:
+            # update positions
+            self.state[:, :2] += dt * self.state[:, 2:]
         
-        # update positions
-        self.state[:, :2] += dt * self.state[:, 2:]
-        
-        #check for fish hitting the trap
-        dist_arr = cdist(self.state[:,:2], np.array(list(zip(self.perimeter[0], self.perimeter[1]))))
-        hit_trap = (dist_arr.min(axis=1) < self.size)
-        self.state[hit_trap, 2:] *= -1
+            #check for fish hitting the trap
+            dist_arr = cdist(self.state[:,:2], np.array(list(zip(self.perimeter[0], self.perimeter[1]))))
+            hit_trap = (dist_arr.min(axis=1) < self.size)
+            for i in range(0, len(dist_arr)):
+                if(self.perimeter[1,i]<self.bounds[3] - 1):
+                    hit_trap[i] = False
+            self.state[hit_trap, 2:] *= -1
         
         #print(dist_arr.shape)
         
-        # check for crossing boundary
-        crossed_x1 = (self.state[:, 0] < self.bounds[0] + self.size)
-        crossed_x2 = (self.state[:, 0] > self.bounds[1] - self.size)
-        crossed_y1 = (self.state[:, 1] < self.bounds[2] + self.size)
-        crossed_y2 = (self.state[:, 1] > self.bounds[3] - self.size)
+            # check for crossing boundary
+            crossed_x1 = (self.state[:, 0] < self.bounds[0] + self.size)
+            crossed_x2 = (self.state[:, 0] > self.bounds[1] - self.size)
+            crossed_y1 = (self.state[:, 1] < self.bounds[2] + self.size)
+            crossed_y2 = (self.state[:, 1] > self.bounds[3] - self.size)
 
-        self.state[crossed_x1, 0] = self.bounds[1] - self.size
-        self.state[crossed_x2, 0] = self.bounds[0] + self.size
+            self.state[crossed_x1, 0] = self.bounds[1] - self.size
+            self.state[crossed_x2, 0] = self.bounds[0] + self.size
 
-        self.state[crossed_y1, 1] = self.bounds[2] + self.size
-        self.state[crossed_y2, 1] = self.bounds[3] - self.size
+            self.state[crossed_y1, 1] = self.bounds[2] + self.size
+            self.state[crossed_y2, 1] = self.bounds[3] - self.size
 
-        #self.state[crossed_x1 | crossed_x2, 2] *= -1
-        self.state[crossed_y1 | crossed_y2, 3] *= -1
-        self.state[crossed_y1, 0] *= -1
-
-        #moving boundary to show tidal movement
-        if(self.bounds[3] > 0):
-            self.bounds[3] = self.bounds[3] - (1/300)
-            self.tide_state[1,:] = self.bounds[3]
+            #self.state[crossed_x1 | crossed_x2, 2] *= -1
+            self.state[crossed_y1 | crossed_y2, 3] *= -1
+            self.state[crossed_y1, 0] *= -1
+        
+            #moving boundary to show tidal movement
+            if self.tide_movement_up:
+                self.bounds[3] = self.bounds[3] + (1/300)
+                self.tide_state[1,:] = self.bounds[3]
+                if self.bounds[3] >= 2:
+                    self.tide_movement_up = False
+            else:
+                if(self.bounds[3] > 0):
+                    self.bounds[3] = self.bounds[3] - (1/300)
+                    self.tide_state[1,:] = self.bounds[3]
+                else:
+                    self.end = True
+                    self.frozen_iter += 1
             
             
 def init():
     """initialize animation"""
     global box, rect
-    particles.set_data([], [])
+    free_fish.set_data([], [])
     tide.set_data([], [])
     perimeter.set_data([],[])
     rect.set_edgecolor('none')
-    return particles, tide, perimeter, rect
+    trapped_fish.set_data([], [])
+    return free_fish, tide, perimeter, rect, trapped_fish
 
 def animate(i):
     """perform animation step"""
@@ -108,11 +130,29 @@ def animate(i):
     
     # update pieces of the animation
     rect.set_edgecolor('k')
-    particles.set_data(box.state[:, 0], box.state[:, 1])
-    particles.set_markersize(ms)
+    # free_fish.set_color('blue')
     tide.set_data(box.tide_state)
+    perimeter = box.perimeter
+    
     perimeter.set_data(box.perimeter)
-    return particles, tide, rect
+    
+    x = box.state[:, 0]
+    y = box.state[:, 1]
+    
+    df = pd.DataFrame({'x':x, 'y':y})
+    in_df = df[df[['x', 'y']].apply(lambda row : row['x']**2 + row['y']** 2 <= 1 and row['y'] <= 0, axis=1)]
+    out_df = df[df[['x','y']].apply(lambda row : row['x']**2 + row['y']**2 > 1 or row['y'] > 0, axis=1)]
+
+    free_fish.set_data(np.array(out_df.x), np.array(out_df.y))
+    free_fish.set_markersize(ms)
+    
+    if self.frozen_iter < 15:
+        trapped_fish.set_data(np.array(in_df.x), np.array(in_df.y))
+    else
+        trapped_fish.set_date([],[])
+    trapped_fish.set_markersize(ms)
+    
+    return free_fish, tide, rect, trapped_fish
 
 if __name__ == "__main__":
 
@@ -120,28 +160,36 @@ if __name__ == "__main__":
     # set up initial state
     np.random.seed(0)
     init_state = -0.5 + np.random.random((50, 4))
-    init_state[:, :2] *= 3.9
+    init_state[:, 0] *= 3.9
+    init_state[:, 1] *= 0.3
+    init_state[:, 1] -= 1.6
 
     box = ParticleBox(init_state, size=0.04)
     dt = 1. / 30 # 30fps
 
-
     #------------------------------------------------------------
     # set up figure and animation
     fig = plt.figure()
+    fig.suptitle('Aerial View - Animation of Trap', fontsize=16)
     fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
     ax = fig.add_subplot(111, aspect='equal', autoscale_on=False,
                          xlim=(-3.2, 3.2), ylim=(-2.4, 2.4))
     ax.axis('off')
 
-    # particles holds the locations of the particles
-    particles, = ax.plot([], [], 'o', color='salmon', ms=6)
+    # free_fish holds the locations of the fish 'particle' not trapped
+    free_fish, = ax.plot([], [], 'o', color='salmon', ms=6, label='free fish')
+
+    trapped_fish, = ax.plot([], [], 'o', color='black', ms=6, label='captured fish')
 
     # tide holds the tide level for each timestep
-    tide, = ax.plot([], [], '-', color='blue', ms=2)
+    tide, = ax.plot([], [], '-', color='blue', ms=2, label='edge of tide')
 
     # perimeter holds the trap
+    perimeter_right, = ax.plot([],[], '-', color='grey', ms=2, label='trap above water')
+    perimeter_left, = ax.plot([],[], '-', color='grey', ms=2)
     perimeter, = ax.plot([],[], '-', color='grey', ms=2)
+    
+    plt.legend(loc='lower right')
 
     # rect is the box edge
     rect = plt.Rectangle(box.bounds[::2],
@@ -154,7 +202,7 @@ if __name__ == "__main__":
 
     # plt.rcParams["animation.html"] = "jshtml"
 
-    ani = animation.FuncAnimation(fig, animate, frames=700,
+    ani = animation.FuncAnimation(fig, animate, frames=1900,
                                   interval=10, blit=True, init_func=init)
 
     plt.close()
